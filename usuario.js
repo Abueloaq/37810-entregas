0622fe6e9bda2cfe8fcf8f3ea7389b4bf64b8bd0
Abueloaq <jorgewillebald@gmail.com>
2022-05-10 21:06:05 -0300

    Entrega NavBar.js

---
+++ usuario.js
@@ -0,0 +1,13 @@
+
+let data = [1, 2, 3]
+
+let encontrar = (e) => {
+    for (i=0; i=e.length; i++) {
+        if (e[i]>1){
+            console.log(e[i]);
+            break;
+        }
+    }
+}
+
+data.encontrar();
